51fa2cf7e1ffa3946bd2220b0f12defacea7a923
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-10-27 15:23:24 +0000

    feat: improve base tokenizer, add HF abstract

--- torchTextClassifiers/tokenizers/base.py
+++ torchTextClassifiers/tokenizers/base.py
@@ -2,7 +2,46 @@ from abc import ABC, abstractmethod
 
 
 class BaseTokenizer(ABC):
+    def __init__(self, vocab_size: int):
+        self.vocab_size = vocab_size
+
     @abstractmethod
     def tokenize(self, text: str) -> list:
         """Tokenizes the raw input text into a list of tokens."""
+        pass
+
+    def __len__(self):
+        return self.vocab_size
+
+
+class HuggingFaceTokenizer(BaseTokenizer, ABC):
+    def __init__(self, vocab_size: int):
+        super().__init__(vocab_size)
+
+        self.trained = False
+        self.tokenizer = None
+
+    def tokenize(self, text: str) -> list:
+        if not self.trained:
+            raise RuntimeError("Tokenizer must be trained before tokenization.")
+
+        return self.tokenizer(
+            text, padding=True, return_tensors="pt"
+        )  # method from PreTrainedTokenizerFast
+
+    @abstractmethod
+    def train(
+        self,
+        training_corpus: list,
+        save_path: str = None,
+        filesystem=None,
+        s3_save_path=None,
+        **kwargs,
+    ):
+        """Trains the tokenizer on the provided training corpus."""
+        pass
+
+    @abstractmethod
+    def _post_training(self):
+        """Applies post-training configurations to the tokenizer."""
         pass
