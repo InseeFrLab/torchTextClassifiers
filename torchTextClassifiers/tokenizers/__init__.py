ea017f3684248c538f1841b41f24ae19f129c08a
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-11-10 10:56:12 +0000

    feat!(tokenizer): ensure output is consistent across al tokenizers

--- torchTextClassifiers/tokenizers/__init__.py
+++ torchTextClassifiers/tokenizers/__init__.py
@@ -1,10 +1,9 @@
 from .base import (
     HAS_HF as HAS_HF,
 )
-from .base import (
-    BaseTokenizer as BaseTokenizer,
-)
+from .base import BaseTokenizer as BaseTokenizer
 from .base import (
     HuggingFaceTokenizer as HuggingFaceTokenizer,
 )
+from .base import TokenizerOutput as TokenizerOutput
 from .WordPiece import WordPieceTokenizer as WordPieceTokenizer
